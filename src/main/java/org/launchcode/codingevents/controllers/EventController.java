4dc5351f36bc107f0fde19e18ae11e8d29633f06
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-05-08 09:56:33 -0500

    5/8 add display-errors

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -26,6 +26,7 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
@@ -34,7 +35,6 @@ public class EventController {
                                          Errors errors, Model model) {
         if(errors.hasErrors()) {
             model.addAttribute("title", "Create Event");
-            model.addAttribute("errorMsg", "Bad data!");
             return "events/create";
         }
         EventData.add(newEvent);
